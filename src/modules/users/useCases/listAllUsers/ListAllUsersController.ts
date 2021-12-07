62ff6789337c11f7dc3c8ff370c4b2cb736c5ddf
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-07 17:46:35 -0300

    feat: add documentation to the project

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,10 +8,7 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     try {
       const id = request.headers.user_id.toString();
-      console.log(id);
       const users = this.listAllUsersUseCase.execute({ user_id: id });
-      console.log("MATRIZ DE USUARIOS");
-      console.log(users);
       return response.status(200).json(users);
     } catch (e) {
       return response.status(400).json({ error: "Not Allowed" });
